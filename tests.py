5f6d04378405063cb95c32ba1269fd77e7f9dc8b
packetFury <packetfury@pm.me>
2025-10-04 16:29:26 -0500

    Commented out unused unit tests, tweaked formatting.

--- tests.py
+++ tests.py
@@ -16,6 +16,7 @@ print(f"Results from calculator directory: \n{get_files_info('calculator', '../'
 get_file_content() unit tests
 
 from functions.get_file_content import get_file_content
+
 print(f"Results from lorem.txt: {get_file_content('calculator', 'lorem.txt')}")
 
 print(f"Results from main.py: {get_file_content('calculator', 'main.py')}")
@@ -39,10 +40,12 @@ print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 """
 run_python_file() unit tests
-"""
+
 from functions.run_python_file import run_python_file
+
 print(run_python_file("calculator", "main.py"))
 print(run_python_file("calculator", "main.py", ["3 + 5"]))
 print(run_python_file("calculator", "tests.py"))
 print(run_python_file("calculator", "../main.py"))
 print(run_python_file("calculator", "nonexistent.py"))
+"""
